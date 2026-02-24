894c3aca6b4f63313fe57c57bad445267a1e893f
VladZainullin <mittoface02@mail.ru>
2026-02-24 15:47:07 +0300

    Добавляет ожидание закрытия логгера после стопа приложения

--- content/Web/Program.cs
+++ content/Web/Program.cs
@@ -67,6 +67,10 @@ file static class Program
         {
             logger.Fatal(e, "Application not started");
         }
+        finally
+        {
+            await logger.CloseAndFlashAsync();
+        }
 #endif
     }
 }
