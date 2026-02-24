64d2f5838676480c973d1e8c069512cebebb6270
VladZainullin <mittoface02@mail.ru>
2026-02-24 15:25:31 +0300

    Удаляет заголовок Server для Kestrel

--- content/Web/DependencyInjection.cs
+++ content/Web/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Persistence;
 
 namespace Web;
@@ -6,6 +7,11 @@ public static class DependencyInjection
 {
     public static void AddWeb(this IHostApplicationBuilder builder)
     {
+        builder.Services.Configure<KestrelServerOptions>(static kestrelServerOptions =>
+        {
+            kestrelServerOptions.AddServerHeader = false;
+        });
+        
 #if UseMiniProfilerAspNetCore
         builder.Services.AddMiniProfiler().AddEntityFramework();
 #endif
